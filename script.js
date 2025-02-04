d80e6cd7934fd77ca507835731de871316fe5a90
Islam <baydulaev.95@mail.ru>
2025-02-05 01:03:02 +0300

    comit

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
-const gameContainer = document.getElementById("container");
-const clearAllBtn = document.querySelector(".clear");
-const ChangeBtn = document.querySelector(".change");
+const container = document.getElementById("container");
+const ClearButton = document.getElementById("btn");
+const sizeButton = document.getElementById("btnTwo");
 
+// Функция для  рандом цвета йоу
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+// Функция для квадратиков йоу
 function createGrid(size) {
-  gameContainer.innerHTML = "";
-  const cellSize = 100 / size;
-  for (let i = 0; i < size * size; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.style.width = `${cellSize}%`;
-    cell.style.height = `${cellSize}%`;
-    cell.addEventListener("mouseover", () => cell.classList.add('filled'));
-    gameContainer.appendChild(cell);
+  container.innerHTML = '';
+  const totalBlocks = size * size;
+
+//  размер самих квадратов
+  const blockSize = (90 / size);
+
+  for (let i = 0; i < totalBlocks; i++) {
+    const addDiv = document.createElement("div");
+    addDiv.classList.add("box");
+    addDiv.style.width = `${blockSize}%`;
+    addDiv.style.height = `${blockSize}%`;
+    container.appendChild(addDiv);
+
+    addDiv.addEventListener("mouseover", () => {
+      addDiv.style.backgroundColor = getRandomColor();
+    });
   }
 }
 
-createGrid(4);
 
-clearAllBtn.addEventListener("click", () => {
-  document.querySelectorAll(".cell").forEach(cell => cell.classList.remove("filled"));
+
+// При клике на 'Clear' очищаем все типа да
+ClearButton.addEventListener("click", () => {
+  document.querySelectorAll('.box').forEach(item => {
+    item.style.backgroundColor = 'white';
+  });
 });
 
-ChangeBtn.addEventListener("click", () => {
-  let size = parseInt(prompt("Введите желаемый размер"), 10);
-  if (isNaN(size) || size <= 0) {
-    alert("Введите положительное число.");
+// При клике на 'Change Size' запрашиваем нужный размер типа
+sizeButton.addEventListener("click", () => {
+  const newSize = Number(prompt("Пожалуйста, введите размер (до 64):"));
+
+  if (isNaN(newSize) || newSize < 1 || newSize > 64) {
+    alert("Вы ввели  текст или неверный размер блока");
   } else {
-    size = Math.min(size, 100);
-    createGrid(size);
+    createGrid(newSize);
   }
 });
+
+// Изначально с сетка типо 10x10 йоу
+createGrid(10);
